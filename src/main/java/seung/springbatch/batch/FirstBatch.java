d8d1c4b2de42dac2afb5fa45744041b9baffac7b
sean <ngs1214@gmail.com>
2024-08-05 15:50:51 +0900

    FirstJob 실행 (스케줄,컨트롤러)

--- src/main/java/seung/springbatch/batch/FirstBatch.java
+++ src/main/java/seung/springbatch/batch/FirstBatch.java
@@ -1,5 +1,6 @@
 package seung.springbatch.batch;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
@@ -20,15 +21,17 @@ import seung.springbatch.repository.AfterRepository;
 import seung.springbatch.repository.BeforeRepository;
 
 import java.util.Map;
-
 @Configuration
 public class FirstBatch {
+
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
+
     private final BeforeRepository beforeRepository;
     private final AfterRepository afterRepository;
 
     public FirstBatch(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager, BeforeRepository beforeRepository, AfterRepository afterRepository) {
+
         this.jobRepository = jobRepository;
         this.platformTransactionManager = platformTransactionManager;
         this.beforeRepository = beforeRepository;
@@ -36,32 +39,31 @@ public class FirstBatch {
     }
 
     @Bean
-    public Job firstJob(){
-        return new JobBuilder("firstJob",jobRepository)
+    public Job firstJob() {
+
+        System.out.println("first job");
+
+        return new JobBuilder("firstJob", jobRepository)
                 .start(firstStep())
-//                .next()
-//                .next()
                 .build();
     }
 
     @Bean
     public Step firstStep() {
-        System.out.println("firstStep");
-        return new StepBuilder("firstStep",jobRepository)
-                //<리더에서 읽을 타입, 라이트에서 쓸 타입>\
-                //chunk 최소 단위
-                //platformTransactionManager는 청크 진행시에 실패 할 떄 다시 셋팅
-                .<BeforeEntity, AfterEntity>chunk(10, platformTransactionManager)
+
+        System.out.println("first step");
+
+        return new StepBuilder("firstStep", jobRepository)
+                .<BeforeEntity, AfterEntity> chunk(10, platformTransactionManager)
                 .reader(beforeReader())
                 .processor(middleProcessor())
                 .writer(afterWriter())
                 .build();
-
-
     }
 
     @Bean
     public RepositoryItemReader<BeforeEntity> beforeReader() {
+
         return new RepositoryItemReaderBuilder<BeforeEntity>()
                 .name("beforeReader")
                 .pageSize(10)
@@ -71,29 +73,28 @@ public class FirstBatch {
                 .build();
     }
 
-
-
     @Bean
     public ItemProcessor<BeforeEntity, AfterEntity> middleProcessor() {
+
         return new ItemProcessor<BeforeEntity, AfterEntity>() {
 
             @Override
             public AfterEntity process(BeforeEntity item) throws Exception {
+
                 AfterEntity afterEntity = new AfterEntity();
                 afterEntity.setUsername(item.getUsername());
+
                 return afterEntity;
             }
         };
     }
 
-
     @Bean
     public RepositoryItemWriter<AfterEntity> afterWriter() {
+
         return new RepositoryItemWriterBuilder<AfterEntity>()
                 .repository(afterRepository)
                 .methodName("save")
                 .build();
     }
-
-
 }
